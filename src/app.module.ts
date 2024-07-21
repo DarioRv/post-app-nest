4185ab791427ba01ac1033c9dd94589edec38f96
Darío Rubén Vidal <96150592+DarioRv@users.noreply.github.com>
2024-07-21 20:02:41 -0300

    Eliminar archivos sin usar

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
@@ -20,7 +18,7 @@ import { PostsModule } from './posts/posts.module';
     UsersModule,
     PostsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
